aca49beb3221d13cca86dd6cf9146ffe9f3f4792
jschles-sooryen <john.schlesinger@sooryen.com>
2022-01-17 15:26:09 -0500

    Server side authentication function

---
+++ lib/auth.ts
@@ -0,0 +1,24 @@
+import { getSession } from 'next-auth/react';
+import { wrapper } from '../store';
+import { setUser } from '../store/auth/reducer';
+
+export const serverSideAuthentication = () =>
+    wrapper.getServerSideProps((store) => async (ctx): Promise<any> => {
+        const session = await getSession({ req: ctx.req });
+
+        if (!session) {
+            store.dispatch(setUser(null));
+            return {
+                redirect: {
+                    destination: '/signin',
+                    permanent: false,
+                },
+            };
+        }
+
+        store.dispatch(setUser(session.user));
+
+        return {
+            props: { session },
+        };
+    });
