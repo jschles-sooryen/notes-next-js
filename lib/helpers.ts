0d50b906da2965940fe89fbbcc98516ea0c3b3dc
jschles-sooryen <john.schlesinger@sooryen.com>
2022-01-24 18:30:19 -0500

    Format date helper

---
+++ lib/helpers.ts
@@ -0,0 +1,27 @@
+import { format } from 'date-fns';
+
+export const formatDate = (
+    date: Date | string | undefined,
+    isDetail = false
+): string => {
+    const dateObj = date ? new Date(date) : new Date();
+
+    if (isDetail) {
+        return format(dateObj, "MMMM do yyyy 'at' h:mm a");
+    }
+
+    let dateFormat = 'MM/dd/yyyy';
+    let formattedDate;
+
+    if (dateObj.getDate() === new Date().getDate()) {
+        dateFormat = 'h:mm a';
+    }
+
+    if (dateObj.getDate() === new Date().getDate() - 1) {
+        formattedDate = 'Yesterday';
+    } else {
+        formattedDate = format(dateObj, dateFormat);
+    }
+
+    return formattedDate;
+};
