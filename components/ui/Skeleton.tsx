cc2de87d1e57e9aa6fa9a98865fc296e85bff5d8
jschles-sooryen <john.schlesinger@encora.com>
2022-02-02 20:50:52 -0500

    Add width prop to skeleton

--- components/ui/Skeleton.tsx
+++ components/ui/Skeleton.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
-import { Skeleton as MuiSkeleton } from '@mui/material';
+import { Skeleton as MuiSkeleton, SkeletonProps } from '@mui/material';
 import { selectIsLoading } from '../../store/loading/selectors';
 
-interface Props {
+interface Props extends SkeletonProps {
     children?: React.ReactNode;
+    width?: string;
 }
 
-const Skeleton: React.FC<Props> = ({ children }) => {
+const Skeleton: React.FC<Props> = ({ children, width = '100%' }) => {
     const isLoading = useSelector(selectIsLoading);
     return isLoading ? (
-        <MuiSkeleton animation="wave" width="100%" />
+        <MuiSkeleton animation="wave" width={width} />
     ) : (
         <>{children}</>
     );
