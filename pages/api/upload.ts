8f7c801d38c03f17dda2546750c872c70b0b8103
jschles-sooryen <john.schlesinger@encora.com>
2022-02-10 18:05:52 -0500

    Image upload to S3

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+import aws from 'aws-sdk';
 import multiparty from 'multiparty';
 import fs, { ReadStream } from 'fs';
 
@@ -11,17 +12,47 @@ export const config = {
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession({ req });
-    if (session) {
-        const form = new multiparty.Form();
-        let uploadFile: ReadStream;
-        form.parse(req, (err, fields, files) => {
-            // TODO: Handle error
-            if (files.upload) {
-                uploadFile = fs.createReadStream(files.upload[0].path);
-            }
-        });
-        if (req.method === 'POST') {
-            res.status(200).json({ message: 'Hello' });
+    if (session && req.method === 'POST') {
+        try {
+            aws.config.update({
+                credentials: {
+                    accessKeyId: process.env.AWS_ACCESS_KEY,
+                    secretAccessKey: process.env.AWS_SECRET_KEY,
+                },
+                region: process.env.AWS_REGION,
+            });
+
+            const s3 = new aws.S3();
+
+            const form = new multiparty.Form();
+            let uploadFile: ReadStream;
+            let fileName: string;
+
+            form.parse(req, async (uploadError, _, files) => {
+                if (uploadError) {
+                    throw new Error('Error parsing data: ' + uploadError.stack);
+                }
+
+                if (files.upload) {
+                    uploadFile = fs.createReadStream(files.upload[0].path);
+                    fileName = files.upload[0].originalFilename;
+
+                    await s3.upload(
+                        {
+                            Bucket: process.env.AWS_BUCKET_NAME,
+                            Body: uploadFile,
+                            Key: fileName,
+                        },
+                        {},
+                        (awsError, data) => {
+                            if (awsError) throw new Error(awsError.message);
+                            res.status(200).json({ url: data.Location });
+                        }
+                    );
+                }
+            });
+        } catch (e) {
+            res.status(500).json({ message: e.message });
         }
     } else {
         res.status(401).json({ message: 'Unauthenticated' });
