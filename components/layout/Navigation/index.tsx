635f03a0510a1473b89c412b47f48b4249849f41
jschles-sooryen <john.schlesinger@encora.com>
2022-02-13 16:23:37 -0500

    Add folder button

--- components/layout/Navigation/index.tsx
+++ components/layout/Navigation/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import { Box, styled } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import { selectUser } from '../../../store/auth/selectors';
 import UserDropdown from './UserDropdown';
 import NoteSearchInput from '../../notes/NoteSearchInput';
 import FoldersList from '../../folders/FoldersList';
+import Button from '../../ui/Button';
 
 const NavigationContainer = styled(Box)(({ theme }) => ({
     height: '100%',
@@ -21,14 +24,17 @@ const Navigation: React.FC = () => {
     const router = useRouter();
     const user = useSelector(selectUser);
     const hasNotes = router.pathname.includes('notes');
+    const isFoldersPage = router.pathname === '/folders';
     return (
         <NavigationContainer>
             <Box>
                 <UserDropdown user={user} />
                 {hasNotes && <NoteSearchInput />}
             </Box>
-            <FoldersList />
-            <Box>Add New Folder</Box>
+            {!isFoldersPage && <FoldersList isNav />}
+            <NextLink href="/create-folder" passHref>
+                <Button startIcon={<AddIcon />}>Add New Folder</Button>
+            </NextLink>
         </NavigationContainer>
     );
 };
