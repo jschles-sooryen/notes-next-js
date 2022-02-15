d8498413993cdc49e4b9e4d7b67e20bbc936a1eb
jschles-sooryen <john.schlesinger@encora.com>
2022-02-14 20:05:19 -0500

    Fix selected folder not changing when navigating to folder notes page

--- pages/folders/[folderId]/notes/index.tsx
+++ pages/folders/[folderId]/notes/index.tsx
@@ -63,7 +63,7 @@ const NotesPage: NextPage = () => {
 
     React.useEffect(() => {
         dispatch(fetchNotesInit(folderId));
-    }, []);
+    }, [folderId]);
 
     React.useEffect(() => {
         if (successRedirect) {
