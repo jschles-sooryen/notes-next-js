9ffd300613d66275df2dd274ba7d3d1e76ec6ed7
jschles-sooryen <john.schlesinger@encora.com>
2022-02-14 20:25:34 -0500

    Add collapse to FolderLink

--- components/folders/FolderLink.tsx
+++ components/folders/FolderLink.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import NextLink from 'next/link';
-import { Box, IconButton } from '@mui/material';
+import { Box, Collapse, Grid, IconButton } from '@mui/material';
 import FolderIcon from '@mui/icons-material/FolderRounded';
 import MoreIcon from '@mui/icons-material/MoreHorizRounded';
 import { Folder } from '../../interfaces';
+import Skeleton from '../ui/Skeleton';
+import Button from '../ui/Button';
 
 interface Props extends Omit<Folder, 'user'> {
     isNav?: boolean;
@@ -11,35 +13,74 @@ interface Props extends Omit<Folder, 'user'> {
 
 const FolderButton: React.FC<Props> = ({ _id, name, isNav = false }) => {
     const [isOptionsOpen, setIsOptionsOpen] = React.useState(false);
+
+    const handleIconClick = () => {
+        setIsOptionsOpen((prev) => !prev);
+    };
+
+    const changeColor = isNav ? 'secondary.light' : 'bg.main';
+    const optionButtonVariant =
+        changeColor === 'secondary.light' ? 'bg.main' : 'secondary.light';
+
     return (
-        <Box>
+        <Skeleton>
             <Box
                 sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
                     borderRadius: '4px',
+                    backgroundColor: isOptionsOpen
+                        ? changeColor
+                        : 'transparent',
                     '&:hover': {
-                        backgroundColor: isNav ? 'secondary.light' : 'bg.main',
+                        backgroundColor: changeColor,
                     },
                     paddingX: 1,
                     cursor: 'pointer',
                 }}
             >
-                <NextLink href={`/folders/${_id}/notes`}>
-                    <Box
-                        sx={{ display: 'flex', alignItems: 'center', flex: 1 }}
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <NextLink href={`/folders/${_id}/notes`} passHref>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                flex: 1,
+                            }}
+                        >
+                            <FolderIcon sx={{ marginRight: 1 }} />
+                            {name}
+                        </Box>
+                    </NextLink>
+                    <IconButton
+                        color="primary"
+                        disableRipple
+                        onClick={handleIconClick}
                     >
-                        <FolderIcon sx={{ marginRight: 1 }} />
-                        {name}
-                    </Box>
-                </NextLink>
-                <IconButton color="primary" disableRipple>
-                    <MoreIcon />
-                </IconButton>
+                        <MoreIcon />
+                    </IconButton>
+                </Box>
+                {/* TODO: Options (Update and Delete) */}
+                <Collapse in={isOptionsOpen}>
+                    <Grid container spacing={1} sx={{ paddingBottom: 1 }}>
+                        <Grid item xs={6}>
+                            <Button fullWidth color={optionButtonVariant}>
+                                Update
+                            </Button>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button fullWidth color={optionButtonVariant}>
+                                Delete
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Collapse>
             </Box>
-            {/* TODO: Options (Update and Delete) */}
-        </Box>
+        </Skeleton>
     );
 };
 
