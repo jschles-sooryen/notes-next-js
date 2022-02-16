58a42ba6037b28f904a1e158b938e403fcc4162c
jschles-sooryen <john.schlesinger@encora.com>
2022-02-16 18:52:21 -0500

    NoteDescription adjustments

--- components/notes/NoteDescription.tsx
+++ components/notes/NoteDescription.tsx
@@ -1,18 +1,48 @@
 import * as React from 'react';
+import { Box } from '@mui/material';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '../../ckeditor5/build/ckeditor';
 import EditorContainer from '../ui/EditorContainer';
 
 interface Props {
     value: string;
-    height: string;
 }
 
-const NoteDescription: React.FC<Props> = ({ height, value }) => {
+const NoteDescription: React.FC<Props> = ({ value }) => {
+    const [editorHeight, setEditorHeight] = React.useState('');
+    const rootRef = React.useRef() as any;
+
+    // TODO: Make hook
+    React.useEffect(() => {
+        function changeEditorHeight(): void {
+            setTimeout((): void => {
+                setEditorHeight(
+                    `${rootRef?.current?.getBoundingClientRect().height - 32}px`
+                );
+            }, 0);
+        }
+
+        changeEditorHeight();
+
+        window.addEventListener('resize', changeEditorHeight);
+
+        return () => window.removeEventListener('resize', changeEditorHeight);
+    }, []);
+
     return (
-        <EditorContainer maxHeight={height || '100%'}>
-            <CKEditor editor={ClassicEditor} data={value} disabled />
-        </EditorContainer>
+        <Box
+            ref={rootRef}
+            sx={{
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'start',
+            }}
+        >
+            <EditorContainer maxHeight={editorHeight || '100%'}>
+                <CKEditor editor={ClassicEditor} data={value} disabled />
+            </EditorContainer>
+        </Box>
     );
 };
 
