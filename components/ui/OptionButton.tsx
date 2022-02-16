29d3f8e872fad54336b50c962fa6043999c858f3
jschles-sooryen <john.schlesinger@encora.com>
2022-02-16 18:30:46 -0500

    Modify OptionButton

--- components/ui/OptionButton.tsx
+++ components/ui/OptionButton.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Button, { ButtonProps } from './Button';
+import { SxProps, Theme } from '@mui/material';
 
 interface Props extends Omit<ButtonProps, 'variant'> {
     variant: 'success' | 'warning';
+    sx?: SxProps<Theme>;
 }
 
 const OptionButton: React.FC<Props> = React.forwardRef(function OptionButton(
-    { children, variant, ...other },
+    { children, variant, sx, ...other },
     ref
 ) {
     const color = variant === 'success' ? '#2e7d32' : '#d32f2f';
@@ -15,12 +17,15 @@ const OptionButton: React.FC<Props> = React.forwardRef(function OptionButton(
             color={color}
             ref={ref}
             {...other}
-            sx={{
-                color: 'secondary.light',
-                '&:hover': {
-                    backgroundColor: 'secondary.light',
+            sx={[
+                {
+                    color: 'secondary.light',
+                    '&:hover': {
+                        backgroundColor: 'secondary.light',
+                    },
                 },
-            }}
+                ...(Array.isArray(sx) ? sx : [sx]),
+            ]}
         >
             {children}
         </Button>
