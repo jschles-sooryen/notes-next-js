298ba0853993a9259869b4009dac57c007d8e4cd
jschles-sooryen <john.schlesinger@encora.com>
2022-02-19 16:21:14 -0500

    Add scrolling for notes selection

--- components/ui/SelectionContainer.tsx
+++ components/ui/SelectionContainer.tsx
@@ -5,6 +5,7 @@ const SelectionContainer = styled(Box)(({ theme }) => ({
     padding: `${theme.spacing(3)} ${theme.spacing(2)}`,
     backgroundColor: theme.palette.secondary.light,
     borderRight: `2px solid ${theme.palette.bg.main}`,
+    overflowY: 'auto',
 }));
 
 export default SelectionContainer;
