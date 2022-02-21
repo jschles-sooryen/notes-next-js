e533cd1bf4fabd1d058a9c36e52140cfd9abd44e
jschles-sooryen <john.schlesinger@encora.com>
2022-02-21 13:58:12 -0500

    SelectionContainer changes for mobile

--- components/ui/SelectionContainer.tsx
+++ components/ui/SelectionContainer.tsx
@@ -6,6 +6,14 @@ const SelectionContainer = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.secondary.light,
     borderRight: `2px solid ${theme.palette.bg.main}`,
     overflowY: 'auto',
+    [theme.breakpoints.down('sm')]: {
+        flex: 1,
+        width: 'auto',
+        paddingTop: 0,
+        paddingBottom: 0,
+        borderRight: 'none',
+        height: '100%',
+    },
 }));
 
 export default SelectionContainer;
