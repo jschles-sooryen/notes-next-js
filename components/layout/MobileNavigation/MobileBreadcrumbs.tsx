32e3baaf20db522a1b919ccc2fdc8c414d1ec342
jschles-sooryen <john.schlesinger@encora.com>
2022-02-28 18:20:11 -0500

    Cleanup

--- components/layout/MobileNavigation/MobileBreadcrumbs.tsx
+++ components/layout/MobileNavigation/MobileBreadcrumbs.tsx
@@ -6,9 +6,7 @@ import ArrowIcon from '@mui/icons-material/ArrowBackIos';
 import Link from '@components/ui/Link';
 import { selectSelectedFolder } from '@store/folders/selectors';
 
-interface Props {}
-
-const MobileBreadcrumbs: React.FC<Props> = ({}) => {
+const MobileBreadcrumbs: React.FC = () => {
     const router = useRouter();
     const selectedFolder = useSelector(selectSelectedFolder);
     const folderId = router.query.folderId as string;
