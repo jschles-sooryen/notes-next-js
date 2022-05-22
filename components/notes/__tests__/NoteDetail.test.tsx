be46d8f275201d081c2fc0aaab1206bc4c0a1c76
jschles-sooryen <john.schlesinger@encora.com>
2022-05-22 18:55:16 -0400

    set timeout in beforeAll

--- components/notes/__tests__/NoteDetail.test.tsx
+++ components/notes/__tests__/NoteDetail.test.tsx
@@ -45,6 +45,8 @@ describe('<NoteDetail />', () => {
                 message: 'success',
             },
         }));
+
+        jest.setTimeout(10000);
     });
 
     afterEach(() => {
@@ -150,7 +152,6 @@ describe('<NoteDetail />', () => {
     });
 
     it('Opens delete folder confirmation modal upon clicking "Delete" button', async () => {
-        jest.setTimeout(10000);
         const spy = jest.spyOn(Mutations, 'DELETE_NOTE_MUTATION');
         render(
             <NoteDetail note={note} folderId={note.folder} noteId={note._id} />
